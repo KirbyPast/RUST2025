2b48f0a5b31c130013df3d3033fbc034dca8394c
KirbyPast <iovalex29@gmail.com>
2024-11-03 17:54:46 +0200

    Lab 05 final

--- lab05/p03/src/main.rs
+++ lab05/p03/src/main.rs
@@ -1,7 +1,7 @@
-use std::fs;
 use serde_derive::Deserialize;
+use std::fs;
 
-#[derive(Debug,Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Student {
     name: String,
     phone: String,
@@ -9,7 +9,32 @@ struct Student {
 }
 
 fn main() {
-    let contents = fs::read_to_string("src/students.txt").unwrap();
-    let s: Student = serde_json::from_str(&contents).unwrap();
-    println!("{:?}",s);
+    let contents = fs::read_to_string("src/students.json").unwrap();
+    let s: [Student; 4] = serde_json::from_str(&contents).unwrap();
+
+    let mut idx = 0;
+    let mut youngest = 999;
+    let mut youngest_idx = 0;
+    let mut oldest = -1;
+    let mut oldest_idx = 0;
+
+    while idx < 4 {
+        if s[idx].age > oldest {
+            oldest_idx = idx;
+            oldest = s[idx].age;
+        }
+        if s[idx].age < youngest {
+            youngest_idx = idx;
+            youngest = s[idx].age;
+        }
+        idx = idx + 1;
+    }
+    println!(
+        "{:?} is the oldest student, aged {:?}",
+        s[oldest_idx].name, s[oldest_idx].age
+    );
+    println!(
+        "{:?} is the youngest student, aged {:?}",
+        s[youngest_idx].name, s[youngest_idx].age
+    );
 }
