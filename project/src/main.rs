8cc89f271c7ecf9daff0a951c1f18bdc44955615
KirbyPast <iovalex29@gmail.com>
2024-12-16 21:13:42 +0200

    Custom split size detection & calculation

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-
+static SPLIT_SIZE : u32 =  1024 * 1024; //Default split size is 1mB. 1024 bytes = 1kB * 1024 bytes = 1mB
 
 fn incorrect_usage(args : Vec<String>) {
     println!("Error! Invalid command!");
@@ -8,46 +8,87 @@ fn incorrect_usage(args : Vec<String>) {
     {:?} unsplit (file_path)", 
         args[0], args[0]);
 }
-fn main() {
+
+fn update_split_size(multiplier : u32, multiplier_size : String) -> Result<u32,String> {
+    let mut size: u32 = 1024 * 1024; //Default is 1mb
+    match multiplier_size.to_lowercase().as_str() {
+        "b" => {
+            size = size / (1024 * 1024) * multiplier;
+            return Ok(size);
+        }
+        "kb" => {
+            size = size / 1024 * multiplier;
+            return Ok(size);
+        }
+        "mb" => {
+            size = size * multiplier;
+            return Ok(size);
+        }
+        "gb" => {
+            size = size * 1024 * multiplier;
+            return Ok(size);
+        }
+        _ => {
+            println!("Invalid format of size!!!");
+            return Err(String::from("Invalid format"));
+        }
+    }
+}
+fn main() -> Result<(), String>{
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("Usage: {:?} split (file_path) [-s [num]b/kb/mb/gb] 
                 OR 
                 {:?} unsplit (file_path)", 
                     args[0], args[0]);
+        return Err(String::from("Invalid usage!"));
     }
-    else {
+    else {  
         //let program_path: &String = &args[0];
         let command: &String = &args[1];
 
         match command.as_str() {
             "split" => {
                 println!("Detected split command!");
-                println!("{:?}", args.len());
-                if args.len() > 3 {
+                if args.len() > 4 {
                     //Means client has probably changed size.
-                    let size_check = &args[3];
-                    println!("{size_check}");
+                    let size_check: &String = &args[3];
                     match size_check.as_str() {
                         "-s" => {
-                            println!("Correct usage!");
+                            let mut multiplier_string : String = String::from("");
+                            let mut multiplier_size : String = String::from("");
+                            let new_size: &String = &args[4];
+                            for i in new_size.chars() {
+                                if !i.is_digit(10) {
+                                    multiplier_size.push(i);
+                                }
+                                else {
+                                    multiplier_string.push(i);
+                                }
+                            }
+                            let multiplier : u32 = multiplier_string.parse().unwrap();
+                            let x = update_split_size(multiplier, multiplier_size)?;
+                            println!("{x}");
+                            return Ok(());
                         }
                         _ => {
                             //Doesn't matter args is destroyed when entering function since program is over.
                             incorrect_usage(args);
-                            return;
+                            return Err(String::from("Incorrect usage"));
                         }
                     }
                 }
             }
             "unsplit" => {
                 println!("Detected unsplit command!");
+                return Ok(())
             }
             _ => {
                 //Doesn't matter args is destroyed when entering function since program is over.
                 incorrect_usage(args);
-                return;
+                return Err(String::from("Incorrect usage"));
             }
         }
     }
+    return Ok(());
 }
