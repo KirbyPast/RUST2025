ebc2c9e0f2d1c5abb566ff414fd9ffe4b73adf28
KirbyPast <iovalex29@gmail.com>
2024-12-16 20:35:31 +0200

    Command recognition

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,10 +1,53 @@
 use std::env;
 
+
+fn incorrect_usage(args : Vec<String>) {
+    println!("Error! Invalid command!");
+    println!("Usage: {:?} split (file_path) [-s [num]b/kb/mb/gb] 
+    OR 
+    {:?} unsplit (file_path)", 
+        args[0], args[0]);
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() >= 2 {
-        let prog_name: &String = &args[0];
+    if args.len() < 3 {
+        println!("Usage: {:?} split (file_path) [-s [num]b/kb/mb/gb] 
+                OR 
+                {:?} unsplit (file_path)", 
+                    args[0], args[0]);
+    }
+    else {
+        //let program_path: &String = &args[0];
         let command: &String = &args[1];
-        println!("name: {:?}, command: {:?} \n", prog_name, command);
+
+        match command.as_str() {
+            "split" => {
+                println!("Detected split command!");
+                println!("{:?}", args.len());
+                if args.len() > 3 {
+                    //Means client has probably changed size.
+                    let size_check = &args[3];
+                    println!("{size_check}");
+                    match size_check.as_str() {
+                        "-s" => {
+                            println!("Correct usage!");
+                        }
+                        _ => {
+                            //Doesn't matter args is destroyed when entering function since program is over.
+                            incorrect_usage(args);
+                            return;
+                        }
+                    }
+                }
+            }
+            "unsplit" => {
+                println!("Detected unsplit command!");
+            }
+            _ => {
+                //Doesn't matter args is destroyed when entering function since program is over.
+                incorrect_usage(args);
+                return;
+            }
+        }
     }
 }
